c142ee9fe789c507ede0c6a80eb6ff5aee72b78f
kunalverma <vermakunal15092000@gmail.com>
2023-07-20 18:31:12 +0530

    hello

---
+++ pages/assets/js/navbar.js
@@ -0,0 +1,9 @@
+function openNav() {
+  let x = document.getElementById("sideNav");
+  x.classList.remove("hidden");
+}
+
+function closeNav() {
+  let x = document.getElementById("sideNav");
+  x.classList.add("hidden");
+}
